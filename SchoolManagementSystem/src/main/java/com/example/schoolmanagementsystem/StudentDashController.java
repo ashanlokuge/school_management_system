1b56949239d3247870212a49a2dab05afb2c2210
Ashan Lokuge <ashanlokuge10@gmail.com>
2025-03-16 19:53:09 +0530

    added student-dashboard UI added that method to StudentDashController

---
+++ SchoolManagementSystem/src/main/java/com/example/schoolmanagementsystem/StudentDashController.java
@@ -0,0 +1,17 @@
+package com.example.schoolmanagementsystem;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class StudentDashController {
+    public Button addButton;
+    public Button viewButton;
+
+    public void addOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void ViewOnAction(ActionEvent actionEvent) {
+
+    }
+}
